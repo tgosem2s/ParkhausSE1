bd1af29c3c5d6e82bb4e6f906a993b4d17c30893
Timo Gosemann <gosemann-t@web.de>
2019-04-17 19:29:10 +0200

    First Method Commit

---
+++ src/se1/IstSortiertABC.java
@@ -0,0 +1,42 @@
+package se1;	
+
+import java.util.Arrays;
+
+public class IstSortiertABC implements IstSortiertInterface {
+	
+	public String[][] abhang;
+	
+	@Override
+	public boolean abhaengigkeit(String a, String b, String[][] c) {
+		for (int i = 0; i<c.length;i++) {
+			int aIndex = Arrays.asList(c[i]).indexOf(a);
+			int bIndex = Arrays.asList(c[i]).indexOf(b);
+			if(aIndex >= 0 && bIndex >= 0 && aIndex>bIndex) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	@Override
+	public boolean isWellSorted(String[] sequence) {
+		for(int i = 0; i<sequence.length-1;i++) {
+			if(abhaengigkeit(sequence[i], sequence[i+1], abhang)==false) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	@Override
+	public void setAbhang(String[][] c) {
+		abhang = c;
+		
+	}
+	
+	@Override
+	public String[][] getAbhang() {
+		return abhang;
+	}
+	
+}
--- src/se1/Test.java
+++
@@ -1,5 +0,0 @@
-package se1;
-
-public class Test {
-
-}
